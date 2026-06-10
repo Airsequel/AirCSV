2e7e5a4a39aff92a64d7a2ab8951a3f15585be4b
Adrian Sieber <mail@adriansieber.com>
2026-06-10 14:49:30 +0000

    Set a default window size for new documents

--- AirCSV/AirCSV/AirCSVApp.swift
+++ AirCSV/AirCSV/AirCSVApp.swift
@@ -6,5 +6,6 @@ struct AirCSVApp: App {
     DocumentGroup(viewing: CSVViewModel.self) { configuration in
       ContentView(viewModel: configuration.document)
     }
+    .defaultSize(width: 1000, height: 660)
   }
 }
